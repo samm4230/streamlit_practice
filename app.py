773151689cf3dbdf8c8407ae7f766ad6e780f585
samm4230 <102031809+samm4230@users.noreply.github.com>
2022-03-21 16:06:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-pip install matplotlib
-
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 url="https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv"
 df = pd.read_csv(url)
 
-plot = sns.pairplot(df)
-st.pyplot(plot)
+plot = st.line_chart(df)
+plot
